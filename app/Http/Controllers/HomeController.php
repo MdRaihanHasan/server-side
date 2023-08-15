e0b0e6766bbb4a3e360c742411f5a4ea52db0d96
Raihan <itsrahul880@gmail.com>
2023-08-15 18:45:20 +0600

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,10 +29,10 @@ class HomeController extends Controller
 
         if(!empty($domain_content)){
             // $domain_identity_id = $domain_content->id;
+
             return view('index', compact('domain_content'));
 
         } else {
-
             return response()->json(['message' => 'ektu pore asio'], 400);
         }
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -108,8 +108,10 @@ https://templatemo.com/tm-568-digimedia
   </footer> --}}
 
 
+
   <!-- Scripts -->
   <script src="vendor/jquery/jquery.min.js"></script>
+  
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/js/owl-carousel.js"></script>
   <script src="assets/js/animation.js"></script>
